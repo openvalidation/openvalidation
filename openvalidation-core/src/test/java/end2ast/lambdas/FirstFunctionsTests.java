fc7472a5dc6c2d07d47a95d72d1338b5d54ea109
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2019-09-25 14:10:06 +0200

    refactored previously unused methods in assertion framework added parent method in assertion framework added end2ast lambda test

--- openvalidation-core/src/test/java/end2ast/lambdas/FirstFunctionsTests.java
+++ openvalidation-core/src/test/java/end2ast/lambdas/FirstFunctionsTests.java
@@ -22,8 +22,23 @@ import org.junit.jupiter.api.Test;
 
 public class FirstFunctionsTests {
 
+  /*
+  todo evtl first 10 items -->OF
+  todo sorting arrays
+
+  die ersten 10 items/streets
+  take 10 items from addresses
+  take 10 street from addresses
+  take 10 items from addresses with city gleich dortmund
+  take 10 street from addresses with city gleich dortmund
+
+
+
+
+   */
+
   @Test
-  public void simple_first_function() throws Exception {
+  public void first_function_simple() throws Exception {
     String rule = "a first item from addresses as a first address";
     String schema = "{addresses:[]}";
 
@@ -43,5 +58,32 @@ public class FirstFunctionsTests {
                 .first()
                 .property("addresses")
                 .hasType(DataPropertyType.Array));
+  }
+
+  @Test
+  public void first_function_with_simple_condition() throws Exception {
+    String rule = "a first item from addresses with zip_code equals 12345 as a first address";
+    String schema = "{addresses:[{zip_code: 1, city: Berlin}]}";
+
+    End2AstRunner.run(
+        rule,
+        schema,
+        r ->
+            r.variables()
+                .hasSizeOf(1)
+                .first()
+                  .hasName("a first address")
+                  .operandFunction()
+                    .hasName("FIRST")
+                    .hasType(DataPropertyType.Object)
+                    .sizeOfParameters(1)
+                    .parameters()
+                      .first()
+                        .property("addresses")
+                          .hasType(DataPropertyType.Array)
+                    .parentList()
+                      .second()
+                       // .co
+    );
   }
 }
