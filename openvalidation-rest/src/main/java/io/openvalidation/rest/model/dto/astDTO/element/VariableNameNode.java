79db5c6aded8d3e65bc45f00b19ff42a856684ca
nlueg <nicolueg@yahoo.de>
2019-09-26 07:55:38 +0200

    added variableNameNode, added connector for conditions, added linting for errors that can't be mapped to a specific element

---
+++ openvalidation-rest/src/main/java/io/openvalidation/rest/model/dto/astDTO/element/VariableNameNode.java
@@ -0,0 +1,22 @@
+package io.openvalidation.rest.model.dto.astDTO.element;
+
+import io.openvalidation.rest.model.dto.astDTO.GenericNode;
+import io.openvalidation.rest.model.dto.astDTO.transformation.DocumentSection;
+
+public class VariableNameNode extends GenericNode {
+    private String name;
+
+    public VariableNameNode(DocumentSection section, String name) {
+        super.initializeElement(section);
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
