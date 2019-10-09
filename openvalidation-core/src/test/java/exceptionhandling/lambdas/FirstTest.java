2972c72e5a8c4e706e2bc8fcdbd94d4c7d97c212
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2019-10-09 12:13:50 +0200

    added validator for ASTOperandFunctions

---
+++ openvalidation-core/src/test/java/exceptionhandling/lambdas/FirstTest.java
@@ -0,0 +1,19 @@
+package exceptionhandling.lambdas;
+
+import exceptionhandling.ExceptionRunner;
+
+public class FirstTest {
+
+  private ExceptionRunner runner = new ExceptionRunner();
+
+  //    @ParameterizedTest
+  //    @ValueSource(strings = {"FIRST from AS var"})
+  //    void missing_array_property(String rule) throws Exception {
+  //        runner.run(
+  //                rule,
+  //                "{numbers: [1,2,3]}",
+  //                r -> {
+  //                    r.containsValidationMessage("???");
+  //                });
+  //    }
+}
--- openvalidation-generation/src/test/java/io/openvalidation/generation/tests/lambdas/GeneratorLastTest.java
+++ openvalidation-generation/src/test/java/io/openvalidation/generation/tests/lambdas/GeneratorLastTest.java
@@ -7,14 +7,13 @@ import io.openvalidation.common.ast.builder.ASTOperandFunctionBuilder;
 import io.openvalidation.common.ast.condition.ASTConditionConnector;
 import io.openvalidation.generation.tests.ExpectationBuilder;
 import io.openvalidation.generation.tests.GTE;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
 public class GeneratorLastTest {
-        private static Stream<Arguments> last_test_with_specific_amount() {
+  private static Stream<Arguments> last_test_with_specific_amount() {
     return ExpectationBuilder.newExpectation()
         .javaResult("huml.LAST(model.getAddresses(), 1.0)")
         .toStream();
@@ -22,13 +21,11 @@ public class GeneratorLastTest {
 
   @ParameterizedTest(name = GTE.PARAM_TEST_NAME)
   @MethodSource()
-  public void last_test_with_specific_amount(String language, String expected)
-      throws Exception {
+  public void last_test_with_specific_amount(String language, String expected) throws Exception {
     GTE.execute(
         expected,
         language,
         p -> {
-
           String[] addressPath = {"addresses"};
 
           ASTOperandFunctionBuilder builder = new ASTOperandFunctionBuilder();
@@ -40,29 +37,31 @@ public class GeneratorLastTest {
 
   private static Stream<Arguments> last_test_with_condition() {
     return ExpectationBuilder.newExpectation()
-        .javaResult("huml.LAST(huml.WHERE(model.getAddresses(), x -> huml.EQUALS(x.getCity(), \"Berlin\")))")
+        .javaResult(
+            "huml.LAST(huml.WHERE(model.getAddresses(), x -> huml.EQUALS(x.getCity(), \"Berlin\")))")
         .toStream();
   }
 
   @ParameterizedTest(name = GTE.PARAM_TEST_NAME)
   @MethodSource()
-  public void last_test_with_condition(String language, String expected)
-      throws Exception {
+  public void last_test_with_condition(String language, String expected) throws Exception {
     GTE.execute(
         expected,
         language,
         p -> {
-            ASTConditionBuilder conditionBuilder = new ASTConditionBuilder();
-            conditionBuilder.create().withLeftOperandAsProperty("city")
-                .withRightOperandAsString("Berlin")
-                .withOperator(ASTComparisonOperator.EQUALS);
+          ASTConditionBuilder conditionBuilder = new ASTConditionBuilder();
+          conditionBuilder
+              .create()
+              .withLeftOperandAsProperty("city")
+              .withRightOperandAsString("Berlin")
+              .withOperator(ASTComparisonOperator.EQUALS);
 
           ASTOperandFunctionBuilder builder = new ASTOperandFunctionBuilder();
-          builder.createFunction("LAST")
-            .addParameterAsFunction("WHERE")
-                .addPropertyParameter("addresses")
-                .addLambdaConditionParameter(conditionBuilder.getModel(), "x");
-
+          builder
+              .createFunction("LAST")
+              .addParameterAsFunction("WHERE")
+              .addPropertyParameter("addresses")
+              .addLambdaConditionParameter(conditionBuilder.getModel(), "x");
 
           return builder.getModel();
         });
@@ -70,7 +69,8 @@ public class GeneratorLastTest {
 
   private static Stream<Arguments> last_test_with_condition_and_amount() {
     return ExpectationBuilder.newExpectation()
-        .javaResult("huml.LAST(huml.WHERE(model.getAddresses(), x -> huml.EQUALS(x.getCity(), \"Berlin\")), 1.0)")
+        .javaResult(
+            "huml.LAST(huml.WHERE(model.getAddresses(), x -> huml.EQUALS(x.getCity(), \"Berlin\")), 1.0)")
         .toStream();
   }
 
@@ -82,19 +82,21 @@ public class GeneratorLastTest {
         expected,
         language,
         p -> {
-            ASTConditionBuilder conditionBuilder = new ASTConditionBuilder();
-            conditionBuilder.create().withLeftOperandAsProperty("city")
-                .withRightOperandAsString("Berlin")
-                .withOperator(ASTComparisonOperator.EQUALS);
+          ASTConditionBuilder conditionBuilder = new ASTConditionBuilder();
+          conditionBuilder
+              .create()
+              .withLeftOperandAsProperty("city")
+              .withRightOperandAsString("Berlin")
+              .withOperator(ASTComparisonOperator.EQUALS);
 
           ASTOperandFunctionBuilder builder = new ASTOperandFunctionBuilder();
-          builder.createFunction("LAST")
-            .addParameterAsFunction("WHERE")
-                .addPropertyParameter("addresses")
-                .addLambdaConditionParameter(conditionBuilder.getModel(), "x")
-          .getParentFuncBuilder()
-            .addNumberParameter(1);
-
+          builder
+              .createFunction("LAST")
+              .addParameterAsFunction("WHERE")
+              .addPropertyParameter("addresses")
+              .addLambdaConditionParameter(conditionBuilder.getModel(), "x")
+              .getParentFuncBuilder()
+              .addNumberParameter(1);
 
           return builder.getModel();
         });
@@ -102,7 +104,8 @@ public class GeneratorLastTest {
 
   private static Stream<Arguments> last_test_with_condition_group_and_amount() {
     return ExpectationBuilder.newExpectation()
-        .javaResult("huml.LAST(huml.WHERE(model.getAddresses(), x -> (huml.EQUALS(x.getCity(), \"Berlin\") && huml.NOT_EQUALS(x.getCity(), x.getBirthplace()))), 1.0)")
+        .javaResult(
+            "huml.LAST(huml.WHERE(model.getAddresses(), x -> (huml.EQUALS(x.getCity(), \"Berlin\") && huml.NOT_EQUALS(x.getCity(), x.getBirthplace()))), 1.0)")
         .toStream();
   }
 
@@ -114,27 +117,27 @@ public class GeneratorLastTest {
         expected,
         language,
         p -> {
-            ASTConditionGroupBuilder groupBuilder = new ASTConditionGroupBuilder();
-            groupBuilder.create().
-                    appendCondition(null)
-                    .withLeftOperandAsProperty("city")
-                    .withRightOperandAsString("Berlin")
-                    .withOperator(ASTComparisonOperator.EQUALS)
-            .parentGroup()
-                    .appendCondition(ASTConditionConnector.AND)
-                    .withLeftOperandAsProperty("city")
-                    .withRightOperandAsProperty("birthplace")
-                    .withOperator(ASTComparisonOperator.NOT_EQUALS);
-
+          ASTConditionGroupBuilder groupBuilder = new ASTConditionGroupBuilder();
+          groupBuilder
+              .create()
+              .appendCondition(null)
+              .withLeftOperandAsProperty("city")
+              .withRightOperandAsString("Berlin")
+              .withOperator(ASTComparisonOperator.EQUALS)
+              .parentGroup()
+              .appendCondition(ASTConditionConnector.AND)
+              .withLeftOperandAsProperty("city")
+              .withRightOperandAsProperty("birthplace")
+              .withOperator(ASTComparisonOperator.NOT_EQUALS);
 
           ASTOperandFunctionBuilder builder = new ASTOperandFunctionBuilder();
-          builder.createFunction("LAST")
-            .addParameterAsFunction("WHERE")
-                .addPropertyParameter("addresses")
-                .addLambdaConditionParameter(groupBuilder.getModel(), "x")
-          .getParentFuncBuilder()
-            .addNumberParameter(1);
-
+          builder
+              .createFunction("LAST")
+              .addParameterAsFunction("WHERE")
+              .addPropertyParameter("addresses")
+              .addLambdaConditionParameter(groupBuilder.getModel(), "x")
+              .getParentFuncBuilder()
+              .addNumberParameter(1);
 
           return builder.getModel();
         });
