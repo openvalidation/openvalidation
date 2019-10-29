36016a0ca57286eb9b766489c782615e47a0b176
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2019-10-29 10:32:06 +0100

    edited validator for first and last

--- openvalidation-core/src/main/java/io/openvalidation/core/validation/functions/FunctionFirstValidator.java
+++ openvalidation-core/src/main/java/io/openvalidation/core/validation/functions/FunctionFirstValidator.java
@@ -24,68 +24,46 @@ public class FunctionFirstValidator extends FunctionValidatorBase {
         if (parameters.isEmpty())
             throw new ASTValidationException(
                     "The function " + function.getName() + " requires at least one parameter", function);
-        else {
-            if (parameters.size() <= 3) {
-                // validate first parameter
-                ASTOperandBase firstParam = parameters.get(0);
-                if (!(firstParam instanceof ASTOperandProperty)
-                        && !(firstParam instanceof ASTOperandFunction)
-                            && !(firstParam instanceof ASTOperandVariable)) {
-                    throw new ASTValidationException(
-                            "The function "
-                                    + function.getName()
-                                    + " has to be applied on an array property or a nested function. Currently applied on "
-                                    + firstParam.getClass().getSimpleName(),
-                            function);
-                } else if (firstParam.getDataType() != DataPropertyType.Array) {
-                    throw new ASTValidationException(
-                            "The function "
-                                    + function.getName()
-                                    + " has to be applied on a property of type 'Array'. But is applied on property of type '"
-                                    + firstParam.getDataType()
-                                    + "'",
-                            function);
-                }
-                // FIRST [array/function] [amount/condition]
-                if (parameters.size() == 2) {
-                    ASTOperandBase secondParam = parameters.get(1);
-                    if (!(secondParam instanceof ASTOperandStaticNumber
-                            || secondParam instanceof ASTOperandLambdaCondition)) {
-                        throw new ASTValidationException(
-                                "The function "
-                                        + function.getName()
-                                        + " either takes a number or a lambda condition as the second parameter. Current second parameter is "
-                                        + secondParam.getClass().getSimpleName(),
-                                function);
-                    }
-                    // FIRST [array/function] [condition] [amount]
-                } else if (parameters.size() == 3) {
-                    ASTOperandBase secondParam = parameters.get(1);
-                    if (!(secondParam instanceof ASTOperandLambdaCondition)) {
-                        throw new ASTValidationException(
-                                "The function "
-                                        + function.getName()
-                                        + " takes a lambda condition as the second parameter. Current second parameter is "
-                                        + secondParam.getClass().getSimpleName(),
-                                function);
-                    }
-
-                    ASTOperandBase thirdParam = parameters.get(2);
-                    if (!(thirdParam instanceof ASTOperandStaticNumber
-                            || thirdParam instanceof ASTOperandLambdaCondition)) {
-                        throw new ASTValidationException(
-                                "The function "
-                                        + function.getName()
-                                        + " takes a number or a lambda condition as the third parameter. Current third parameter is "
-                                        + thirdParam.getClass().getSimpleName(),
-                                function);
-                    }
-                }
-            } else {
+        else if (parameters.size() <= 2) {
+            // validate first parameter
+            ASTOperandBase firstParam = parameters.get(0);
+            if (!(firstParam instanceof ASTOperandProperty)
+                    && !(firstParam instanceof ASTOperandFunction)
+                    && !(firstParam instanceof ASTOperandVariable)) {
                 throw new ASTValidationException(
-                        "Invalid number of parameters (" + parameters.size() + ")", function);
+                        "The function "
+                                + function.getName()
+                                + " has to be applied on an array property or a nested function. Currently applied on "
+                                + firstParam.getClass().getSimpleName(),
+                        function);
+            } else if (firstParam.getDataType() != DataPropertyType.Array) {
+                throw new ASTValidationException(
+                        "The function "
+                                + function.getName()
+                                + " has to be applied on a property of type 'Array'. But is applied on property of type '"
+                                + firstParam.getDataType()
+                                + "'",
+                        function);
             }
+            // FIRST [array/function] [amount]
+            if (parameters.size() == 2) {
+                ASTOperandBase secondParam = parameters.get(1);
+                if (!(secondParam instanceof ASTOperandStaticNumber
+                        || secondParam instanceof ASTOperandLambdaCondition)) {
+                    throw new ASTValidationException(
+                            "The function "
+                                    + function.getName()
+                                    + " either takes a number or a lambda condition as the second parameter. Current second parameter is "
+                                    + secondParam.getClass().getSimpleName(),
+                            function);
+                }
+            }
+
+        } else {
+            throw new ASTValidationException(
+                    "Invalid number of parameters (" + parameters.size() + ")", function);
         }
+
 
         validateParameters();
     }
