53eb66928a983803b8f4efe8b6ad42a356765588
nlueg <nicolueg@yahoo.de>
2019-11-06 07:58:03 +0100

    fixed problem of duplicate operandNodes for operations with equal source-text

--- openvalidation-rest/src/main/java/io/openvalidation/rest/model/dto/astDTO/transformation/TransformationHelper.java
+++ openvalidation-rest/src/main/java/io/openvalidation/rest/model/dto/astDTO/transformation/TransformationHelper.java
@@ -3,11 +3,13 @@ package io.openvalidation.rest.model.dto.astDTO.transformation;
 import io.openvalidation.common.ast.condition.ASTCondition;
 import io.openvalidation.common.utils.Constants;
 import io.openvalidation.core.Aliases;
+import io.openvalidation.rest.model.dto.astDTO.TransformationParameter;
 import io.openvalidation.rest.model.dto.astDTO.operation.ConditionNode;
 import io.openvalidation.rest.model.dto.astDTO.operation.OperationNode;
 import java.util.List;
 
 public class TransformationHelper {
+  // Used for a lonely "and" / "or" inside a condition-group which currently can't be parsed properly
   public static ConditionNode getOwnConditionElement(
       String originalSource, ASTCondition condition, String culture) {
     if (originalSource == null || condition == null) return null;
@@ -24,9 +26,13 @@ public class TransformationHelper {
     String newOriginalSource = newOperandString[0].replace("\n", "").trim();
     if (newOriginalSource.isEmpty()) return null;
 
+    List<String> connectionKeywords = Aliases.getAliasByToken(culture, Constants.AND_TOKEN, Constants.OR_TOKEN);
+    if (!connectionKeywords.stream().anyMatch(string -> newOriginalSource.toLowerCase().contains(string.toLowerCase())))
+      return null;
+
     ASTCondition newCondition = new ASTCondition(newOriginalSource);
     DocumentSection newSection = new RangeGenerator(originalSource).generate(newCondition);
 
-    return new OperationNode(newCondition, newSection, culture);
+    return new OperationNode(newCondition, newSection, new TransformationParameter(culture));
   }
 }
