ee5e441aa9915d65d8e2c0492f8190f4e88bee91
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2020-01-08 11:46:05 +0100

    updated last-function validator

--- openvalidation-core/src/test/java/exceptionhandling/lambdas/LastTest.java
+++ openvalidation-core/src/test/java/exceptionhandling/lambdas/LastTest.java
@@ -4,8 +4,10 @@ import exceptionhandling.ExceptionRunner;
 import org.junit.jupiter.api.Test;
 
 public class LastTest {
+
   private ExceptionRunner runner = new ExceptionRunner();
 
+  // applied on property
   @Test
   void one_parameter_missing_array_property() throws Exception {
     runner.run(
@@ -44,17 +46,120 @@ public class LastTest {
                 "The function LAST has to be applied on a property of type 'Array'. But is applied on property of type 'Boolean'"));
   }
 
+  // applied on static
   @Test
   void one_parameter_applied_on_static_string() throws Exception {
     runner.run(
         "LAST from something no one knows about AS var",
         r ->
             r.containsValidationMessage(
-                "The function LAST has to be applied on an array property or a nested function. Currently applied on ASTOperandStaticString"));
+                "The function LAST has to be applied on a property, variable or a nested function of type 'Array'. Currently applied on a string(ASTOperandStaticString)."));
   }
 
   @Test
-  void lambda_condition_with_missing_right_operand() throws Exception {
+  void one_parameter_applied_on_static_number() throws Exception {
+    runner.run(
+        "LAST from 5 AS var",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a property, variable or a nested function of type 'Array'. Currently applied on a number(ASTOperandStaticNumber)."));
+  }
+
+  @Test
+  void one_parameter_applied_with_negative_amount() throws Exception {
+    runner.run(
+        "LAST -1 from numbers AS var",
+        "{numbers: [1,2,3]}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST only takes numbers with a value greater or equal to 1 as the second parameter. Current value is -1.0"));
+  }
+
+  // applied on variables
+  @Test
+  void one_parameter_applied_on_variable_of_type_string() throws Exception {
+    runner.run(
+        "name AS var\n\n" + "LAST from var AS var2",
+        "{name: Peter}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'String'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_of_type_decimal() throws Exception {
+    runner.run(
+        "number AS var\n\n" + "LAST from var AS var2",
+        "{number: 42}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Decimal'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_of_type_bool() throws Exception {
+    runner.run(
+        "married AS var\n\n" + "LAST from var AS var2",
+        "{married: true}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Boolean'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_of_type_object() throws Exception {
+    runner.run(
+        "person AS var\n\n" + "LAST from var AS var2",
+        "{person: {name: Peter}}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Object'"));
+  }
+
+  // applied on variables containing functions
+  @Test
+  void one_parameter_applied_on_variable_containing_function_of_type_string() throws Exception {
+    runner.run(
+        "LAST from names AS var\n\n" + "LAST from var AS var2",
+        "{names: [Peter]}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'String'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_containing_function_of_type_decimal() throws Exception {
+    runner.run(
+        "LAST from numbers AS var\n\n" + "LAST from var AS var2",
+        "{numbers: [1,2,3]}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Decimal'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_containing_function_of_type_bool() throws Exception {
+    runner.run(
+        "LAST from bools AS var\n\n" + "LAST from var AS var2",
+        "{bools: [true, false]}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Boolean'"));
+  }
+
+  @Test
+  void one_parameter_applied_on_variable_containing_function_of_type_object() throws Exception {
+    runner.run(
+        "LAST from staff AS var\n\n" + "LAST from var AS var2",
+        "{staff: [{name: Peter}]}",
+        r ->
+            r.containsValidationMessage(
+                "The function LAST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Object'"));
+  }
+
+  // nesting
+  @Test
+  void nested_where_function_with_lambda_condition_with_missing_right_operand() throws Exception {
     // check if validation of parameters is triggered
     runner.run(
         "LAST from numbers with val greater than AS var",
@@ -63,7 +168,7 @@ public class LastTest {
   }
 
   @Test
-  void lambda_condition_with_missing_left_operand() throws Exception {
+  void nested_where_function_with_lambda_condition_with_missing_left_operand() throws Exception {
     // check if validation of parameters is triggered
     runner.run(
         "LAST from numbers with greater than val AS var",
