b1ce604946605b91a2d5b2a5143d22b19d761da5
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2020-01-08 11:40:44 +0100

    updated error messages for first-function validation to be more specific

--- openvalidation-core/src/test/java/exceptionhandling/lambdas/FirstTest.java
+++ openvalidation-core/src/test/java/exceptionhandling/lambdas/FirstTest.java
@@ -7,7 +7,7 @@ public class FirstTest {
 
   private ExceptionRunner runner = new ExceptionRunner();
 
-  //applied on property
+  // applied on property
   @Test
   void one_parameter_missing_array_property() throws Exception {
     runner.run(
@@ -46,14 +46,14 @@ public class FirstTest {
                 "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Boolean'"));
   }
 
-  //applied on static
+  // applied on static
   @Test
   void one_parameter_applied_on_static_string() throws Exception {
     runner.run(
         "FIRST from something no one knows about AS var",
         r ->
             r.containsValidationMessage(
-                "The function FIRST has to be applied on an array property or a nested function. Currently applied on ASTOperandStaticString"));
+                "The function FIRST has to be applied on a property, variable or a nested function of type 'Array'. Currently applied on a string(ASTOperandStaticString)."));
   }
 
   @Test
@@ -62,7 +62,7 @@ public class FirstTest {
         "FIRST from 5 AS var",
         r ->
             r.containsValidationMessage(
-                "The function FIRST has to be applied on an array property or a nested function. Currently applied on ASTOperandStaticNumber"));
+                "The function FIRST has to be applied on a property, variable or a nested function of type 'Array'. Currently applied on a number(ASTOperandStaticNumber)."));
   }
 
   @Test
@@ -75,97 +75,89 @@ public class FirstTest {
                 "The function FIRST only takes numbers with a value greater or equal to 1 as the second parameter. Current value is -1.0"));
   }
 
-  //applied on variables
+  // applied on variables
   @Test
   void one_parameter_applied_on_variable_of_type_string() throws Exception {
     runner.run(
-        "name AS var\n\n" +
-                "FIRST from var AS var2",
+        "name AS var\n\n" + "FIRST from var AS var2",
         "{name: Peter}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'String'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'String'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_of_type_decimal() throws Exception {
     runner.run(
-        "number AS var\n\n" +
-                "FIRST from var AS var2",
+        "number AS var\n\n" + "FIRST from var AS var2",
         "{number: 42}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Decimal'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Decimal'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_of_type_bool() throws Exception {
     runner.run(
-        "married AS var\n\n" +
-                "FIRST from var AS var2",
+        "married AS var\n\n" + "FIRST from var AS var2",
         "{married: true}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Boolean'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Boolean'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_of_type_object() throws Exception {
     runner.run(
-        "person AS var\n\n" +
-                "FIRST from var AS var2",
+        "person AS var\n\n" + "FIRST from var AS var2",
         "{person: {name: Peter}}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Object'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Object'"));
   }
 
-  //applied on variables containing functions
+  // applied on variables containing functions
   @Test
   void one_parameter_applied_on_variable_containing_function_of_type_string() throws Exception {
     runner.run(
-        "FIRST from names AS var\n\n" +
-                "FIRST from var AS var2",
+        "FIRST from names AS var\n\n" + "FIRST from var AS var2",
         "{names: [Peter]}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'String'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'String'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_containing_function_of_type_decimal() throws Exception {
     runner.run(
-        "FIRST from numbers AS var\n\n" +
-                "FIRST from var AS var2",
+        "FIRST from numbers AS var\n\n" + "FIRST from var AS var2",
         "{numbers: [1,2,3]}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Decimal'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Decimal'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_containing_function_of_type_bool() throws Exception {
     runner.run(
-        "FIRST from bools AS var\n\n" +
-                "FIRST from var AS var2",
+        "FIRST from bools AS var\n\n" + "FIRST from var AS var2",
         "{bools: [true, false]}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Boolean'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Boolean'"));
   }
 
   @Test
   void one_parameter_applied_on_variable_containing_function_of_type_object() throws Exception {
     runner.run(
-        "FIRST from staff AS var\n\n" +
-                "FIRST from var AS var2",
+        "FIRST from staff AS var\n\n" + "FIRST from var AS var2",
         "{staff: [{name: Peter}]}",
         r ->
             r.containsValidationMessage(
-                    "The function FIRST has to be applied on a property of type 'Array'. But is applied on property of type 'Object'"));
+                "The function FIRST has to be applied on a variable of type 'Array'. But is applied on variable of type 'Object'"));
   }
 
-  //nesting
+  // nesting
   @Test
   void nested_where_function_with_lambda_condition_with_missing_right_operand() throws Exception {
     // check if validation of parameters is triggered
