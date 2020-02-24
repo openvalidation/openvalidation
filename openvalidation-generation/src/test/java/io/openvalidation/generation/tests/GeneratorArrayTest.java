cbbdc3307350e4fdd11f65c9738e3590277f0374
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2020-02-24 15:22:50 +0100

    edited generator test to match example from issue

--- openvalidation-generation/src/test/java/io/openvalidation/generation/tests/GeneratorArrayTest.java
+++ openvalidation-generation/src/test/java/io/openvalidation/generation/tests/GeneratorArrayTest.java
@@ -2,19 +2,20 @@ package io.openvalidation.generation.tests;
 
 import io.openvalidation.common.ast.ASTComparisonOperator;
 import io.openvalidation.common.ast.builder.ASTConditionBuilder;
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class GeneratorArrayTest {
   private static Stream<Arguments> array_with_numbers() {
     return Stream.of(
         //                            language      expected
-        Arguments.of("javascript", "huml.LESS_THAN(model.Age, [3.0,4.0,5.0])"),
-        Arguments.of("csharp", "huml.LESS_THAN(model.Age, 3.0,4.0,5.0)"),
-        Arguments.of("java", "huml.LESS_THAN(model.getAge(), 3.0,4.0,5.0)"),
-        Arguments.of("python", "huml.LESS_THAN(model.Age, [3.0,4.0,5.0])"));
+        Arguments.of("javascript", "huml.NONE_OF(model.Age, [3.0,4.0,5.0])"),
+        Arguments.of("csharp", "huml.NONE_OF(model.Age, 3.0,4.0,5.0)"),
+        Arguments.of("java", "huml.NONE_OF(model.getAge(), 3.0,4.0,5.0)"),
+        Arguments.of("python", "huml.NONE_OF(model.Age, [3.0,4.0,5.0])"));
   }
 
   @ParameterizedTest(name = GTE.PARAM_TEST_NAME)
@@ -28,7 +29,7 @@ public class GeneratorArrayTest {
           builder
               .create()
               .withLeftOperandAsProperty("Age")
-              .withOperator(ASTComparisonOperator.LESS_THAN)
+              .withOperator(ASTComparisonOperator.NONE_OF)
               .withRightOperandAsArray(3, 4, 5);
 
           return builder.getModel();
